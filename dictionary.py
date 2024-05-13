dd8578abb5da3986c3e044b0769f203d74486d2a
Odallo <eugeneodali181@gmail.com>
2024-05-13 15:25:19 +0300

    odd list of words

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+#create an empty student dictionary
+student = {}
+
+#input the values
+student['name'] = input ("Enter your name: ")
+student['age'] = int(input ("Enter your age: "))
+
+student['Favourite_color'] = input("Enter your favourite color: ")
+
+#print the dictionary
+print(student)
