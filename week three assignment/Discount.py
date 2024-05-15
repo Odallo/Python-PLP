bca997101e13f419c8c9b19becefed8cca4aa406
Odallo <eugeneodali181@gmail.com>
2024-05-15 10:58:46 +0300

    week three

--- week three assignment/Discount.py
+++ week three assignment/Discount.py
@@ -5,7 +5,9 @@ discount_percent = int(input("Enter the discount percentage: "))
 
 calculate_discount = price * (discount_percent / 100)
 
-if discount_percent > 20:
+if (discount_percent/100) < (20/100) * price:
     result = calculate_discount
 else:
     result = price
+
+print(result)
